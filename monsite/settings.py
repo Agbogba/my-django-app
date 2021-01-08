0164622a1097e4af4fab8691a3285ced2aaa8f73
Agbogba <armandagbogba@hotmail.fr>
2021-01-08 11:01:55 +0100

    nouvelle modification

--- monsite/settings.py
+++ monsite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '0@ynu3j^+0$k3jh0t)sbzg9l2km#fnu!3ey97r-9m1-9^po3(r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+#DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
