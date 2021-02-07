21359d765fac4117ed64fbeeb66b1aa240c4cdfe
Agbogba <armandagbogba@hotmail.fr>
2021-02-07 19:39:55 +0100

    very good

--- monsite/settings.py
+++ monsite/settings.py
@@ -122,7 +122,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 #STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_ROOT = '/home/skillsindatascience/skillsindatascience.pythonanywhere.com/static'
+STATIC_ROOT = '/home/skillsindatascience/skillsindatascience.pythonanywhere.com/blog/static'
 
 #STATICFILES_DIRS = [
 #    os.path.join(BASE_DIR, 'static'),
