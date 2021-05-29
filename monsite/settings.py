5c79d0c042e57eeda19fc04a75670b47f11f4e36
Agbogba <armandagbogba@hotmail.fr>
2021-05-29 18:42:32 +0000

    oulaaaaaa

--- monsite/settings.py
+++ monsite/settings.py
@@ -133,8 +133,8 @@ STATIC_URL = '/static/'
 #STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 #STATIC_ROOT = '/home/skillsindatascience/skillsindatascience.pythonanywhere.com/blog/static'
 
-#MEDIA_URL = '/media/'
-#MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 #MEDIA_ROOT = '/home/skillsindatascience/skillsindatascience.pythonanywhere.com/media'
 
 
