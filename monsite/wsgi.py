92b56a9fb37153da6c68ec601f90b4be416c32f9
Agbogba <armandagbogba@hotmail.fr>
2021-07-01 16:43:21 +0000

    hi

--- monsite/wsgi.py
+++ monsite/wsgi.py
@@ -8,9 +8,16 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
 import os
+import sys
+
+path = '/home/skillsindatascience/skillsindatascience.pythonanywhere.com/monsite'
+if path not in sys.path:
+    sys.path.append(path)
 
 from django.core.wsgi import get_wsgi_application
+from django.contrib.staticfiles.handlers import StaticFilesHandler
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'monsite.settings')
 
-application = get_wsgi_application()
+#application = get_wsgi_application()
+application = StaticFilesHandler(get_wsgi_application())
